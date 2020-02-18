fd4032d01f8016e2573efcd18b719cabb60de5ea
Kelvin Tagoe <tagoe.k@gmail.com>
2020-02-18 18:16:00 -0500

    html completed. Everything is done from this side.

--- exercises/Challenge/prelude.ml
+++ exercises/Challenge/prelude.ml
@@ -13,7 +13,7 @@ let rec unfold (f: 'seed -> ('a * 'seed)) (stop : 'b -> bool) (b : 'seed) : 'a l
 type 'a tree = Node of 'a * ('a tree) list
 
 
-(*Examples*)
+(*List and Tree Examples*)
 let exampleList = [0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10];;
 let exampleTree = Node (1, [ Node (2, [])
 						   						 ; Node (3, [ Node (4, [])
@@ -26,12 +26,20 @@ let exampleTree = Node (1, [ Node (2, [])
 	                   				  				])
 	                      	]);;
 
+(*
+UNFOLD EXAMPLE 1: 
+nats generates a list of natural numbers up to (and including) an inputted maximum integer
+*)
 let nats max = 
 	unfold 
 		(fun b -> b, b + 1)
 		(fun x -> x > max)
 		0
 ;;
+(*
+UNFOLD EXAMPLE 2: 
+nats generates a list of even numbers up to (and including) an inputted maximum integer
+*)
 let evens max =	
 	unfold 
 		(fun b -> (if (b mod 2 = 0) then (b) else (b+1)), b + 2) 
